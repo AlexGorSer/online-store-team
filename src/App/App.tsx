2c09ae63ef28d6a3d4b93d0bf47fe7fb076e4377
AlexGorSer <taskekaminari@gmail.com>
2022-12-26 00:58:18 +0200

    refactor: change structure of the project01

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 
-import Header from "../Header/Header";
-import Footer from "../Footer/Footer";
-import Main from "../Main/Main";
+import Header from "../components/Header/Header";
+import Footer from "../components/Footer/Footer";
+import Main from "../components/Main/Main";
 
 const App = (): React.ReactElement => {
   // Вместо 'one' можно поставить ссылку на картинку
